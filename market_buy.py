8d128a643bfb1bd5944fc7e18f70ff6cd00f406b
FunkyChaper <montanuy.cedric@free.fr>
2016-08-19 13:20:19 +0200

    Update market_buy.py

--- market_buy.py
+++ market_buy.py
@@ -2,9 +2,11 @@
 
 import krakenex
 
+# nouvelle classe API
 k = krakenex.API()
+# appel de la function load_key sur classe k
 k.load_key('kraken.key')
-
+# appel de la fonction query_private (pour passer un ordre AddOrder, sur la pair BTC/EUR, d'achat, au prix du marché, pour un montant de 0.01BTC)
 k.query_private('AddOrder', {'pair': 'XXBTZEUR',
                              'type': 'buy',
                              'ordertype': 'market',
